a7920b572c16f501ee94225a3f08d779bef60f29
Guillem <guillem.orellana@gmail.com>
2017-11-18 23:00:54 +0100

    Fixed errors.

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from Users import GetUsers
-from DatabaseController import MongoController
+
 if __name__ == "__main__":
-    user_list = DownloadUsers.DownloadUsers.download_users("repositories?q=java&sort=stars&order=desc")
+    user_list = GetUsers.DownloadUsers.download_users("repositories?q=java&sort=stars&order=desc")
     for user in user_list:
         print str(user)
         user.save_user()
