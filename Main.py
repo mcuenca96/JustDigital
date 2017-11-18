78c21075870dff332c90154c0898e72bf6a6e8a5
Guillem <guillem.orellana@gmail.com>
2017-11-18 23:00:06 +0100

    Nose

--- Main.py
+++ Main.py
@@ -1,10 +1,7 @@
-from Users import DownloadUsers
+from Users import GetUsers
 from DatabaseController import MongoController
-
 if __name__ == "__main__":
-
     user_list = DownloadUsers.DownloadUsers.download_users("repositories?q=java&sort=stars&order=desc")
-    print len(user_list)
     for user in user_list:
         print str(user)
         user.save_user()
