5b6d8daa271dd9f818d26479609fa91aa85a431a
Marc <cuencamarc96@gmail.com>
2017-11-18 21:48:42 +0100

    mflieb

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from Users import DownloadUsers
 
 if __name__ == "__main__":
-    for user in DownloadUsers.DownloadUsers.download_users("repositories?q=java&sort=stars&order=desc"):
-        print(str(user) + "\n")
+    user_list = DownloadUsers.DownloadUsers.download_users("repositories?q=java&sort=stars&order=desc")
+    for user in user_list:
+        print(str(user) + "\n"
+
--- Users/User.py
+++ Users/User.py
@@ -6,11 +6,12 @@ class User:
         self.location = location if location else "None"
         self.avatar_url = avatar_url if avatar_url else "None"
 
+    def get_login(self):
+        return self.login
+        
     def __str__(self):
         return self.name + '\n' + \
                self.login + '\n' + \
                self.email + '\n' + \
                self.location + '\n' + \
                self.avatar_url + '\n'
-
-
